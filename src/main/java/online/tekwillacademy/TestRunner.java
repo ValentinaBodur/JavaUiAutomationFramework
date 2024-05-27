e6b0e9192e171b4d4bcba66b1e0e6b5605496216
ValentinaBodur <bodur.valentina@gmail.com>
2024-05-27 19:53:26 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,7 +1,32 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        DriverManager manager = DriverManager.getInstance();
+        WebDriver driver = manager.getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+        Thread.sleep(5000);
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://maven.apache.org/download.cgi");
+        Thread.sleep(5000);
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        manager.getDriver().get("https://tekwillacademy-opencart.online/");
+        Thread.sleep(5000);
+        driver.quit();
+
+        System.out.println("The driver is closed");
+
+
     }
+
+
 }
---
+++ src/main/java/online/tekwillacademy/managers/DriverManager.java
@@ -0,0 +1,48 @@
+package online.tekwillacademy.managers;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverManager {
+    private static String webDriverType = "Chrome";
+    private static DriverManager driverManagerSingleObject;
+    private WebDriver driver;
+
+    private DriverManager (){
+        switch (webDriverType.toUpperCase()){
+            case "CHROME":
+                driver = new ChromeDriver();
+                System.out.println("The Chrome Driver was initiated!");
+                break;
+            case "FIREFOX":
+                driver = new SafariDriver();
+                System.out.println("The Safari Driver was initiated!");
+                break;
+            case "EDGE":
+                driver = new EdgeDriver();
+                System.out.println("The Edge Driver was initiated!");
+                break;
+            default:
+                System.out.println("The webDriverType" + webDriverType + "is not de...");
+
+        }
+    }
+    public static DriverManager getInstance(){
+        if (driverManagerSingleObject == null){
+            driverManagerSingleObject = new DriverManager();
+        }
+        return driverManagerSingleObject;
+    }
+
+    public WebDriver getDriver(){
+        if (driver == null){
+            DriverManager.getInstance();
+        }
+        return driver;
+    }
+
+
+}
